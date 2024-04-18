12a77363a0aed0c8229229df59722feca1241e87
christoperBar <cbarnaby@student.ciputra.ac.id>
2024-04-18 11:58:29 +0700

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by MacBook Pro on 18/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            Text("Kuda puki")
+                .font(.subheadline)
+        }
+            
+            
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
