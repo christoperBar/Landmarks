39788efb6959118ab285b5a1304208ee66c3e7c5
christoperBar <cbarnaby@student.ciputra.ac.id>
2024-04-26 09:55:27 +0700

    Chapter 4 Part 3 (MacOS)

---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by MacBook Pro on 26/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+
+#Preview {
+    ContentView()
+        .environment(ModelData())
+}
